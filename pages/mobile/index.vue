92646a8e01d1ec36abf2762b34279ed5b183d37e
Zeng Qihao <zengqihao0104@gmail.com>
2019-07-10 14:03:30 +0800

    dev: handtrack js

--- pages/mobile/index.vue
+++ pages/mobile/index.vue
@@ -1,31 +1,106 @@
 <template lang="pug">
-  .container
+  .inner-page-container
     h1.page-title Motion Detection DEMO
     h2.page-subTitle (Mobile)
     .contents
+      .detection-body
+        video.detection-video(ref="videoEl" autoplay="true")
+        canvas.detection-canvas(ref="handCanvas")
       router-link.button-link(to="/") Back To Top
 </template>
 
 <script>
+  import * as handTrack from 'handtrackjs';
+
+  const modelParams = {
+    flipHorizontal: true,   // flip e.g for video
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.6,    // confidence threshold for predictions.
+  };
+
   export default {
     name: 'mobile-page',
     components: {
+    },
+    methods: {
+      startVideo() {
+        const videoEl = this.$refs.videoEl;
+        if (videoEl) {
+          handTrack.startVideo(videoEl).then((status) => {
+            console.log("video started", status);
+            if (status) {
+              this.runDetection()
+            }
+          });
+        }
+      },
+      runDetection() {
+        const videoEl = this.$refs.videoEl;
+        const handCanvas = this.$refs.handCanvas;
+        const context = handCanvas.getContext("2d");
+        if (videoEl && handCanvas && this.model) {
+          this.model.detect(videoEl).then(predictions => {
+            // console.log("Predictions: ", predictions);
+            this.model.renderPredictions(predictions, handCanvas, context, videoEl);
+            requestAnimationFrame(this.runDetection);
+          });
+        }
+      }
+    },
+    mounted() {
+      const videoEl = this.$refs.videoEl;
+      const handCanvas = this.$refs.handCanvas;
+      if (handTrack && videoEl  && handCanvas) {
+        console.log('handTrack', handTrack);
+        console.log('handCanvas', handCanvas);
+        // Load the model.
+        handTrack.load(modelParams).then(lmodel => {
+          this.model = lmodel;
+          this.startVideo();
+        });
+      }
     }
   }
 </script>
 
 <style lang="sass">
-  .container
+  .inner-page-container
+    position: relative
+    box-sizing: border-box
+    padding: 16px
     margin: 0 auto
     min-height: 100vh
     display: flex
     flex-direction: column
-    justify-content: center
-    align-items: center
     text-align: center
     .contents
       position: relative
       display: block
       width: 100%
       margin: 20px auto
+    .detection-body
+      position: relative
+      width: 100%
+      max-width: 1024px
+      height: calc(100vh - 250px)
+      border: 1px solid #333
+      margin: 0 auto 25px
+    .detection-video
+      position: absolute
+      top: 0
+      left: 0
+      width: 0
+      height: 0
+      z-index: -1
+      display: none
+    .detection-canvas
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0
+      width: 100%
+      height: 100%
+      z-index: 1
 </style>
