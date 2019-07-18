562746443f37c08a72bd6d8570617f8d537d7afb
Zeng Qihao <zengqihao0104@gmail.com>
2019-07-18 14:25:13 +0800

    WIP

---
+++ components/DetectCanvas.vue
@@ -0,0 +1,174 @@
+<template lang="pug">
+  .detection-body(ref="bodyEl")
+    video.detection-video(
+      ref="videoEl"
+      autoplay="true"
+      playsinline
+    )
+    canvas.detection-canvas(
+      ref="canvasEl"
+      class="isDebug"
+    )
+</template>
+
+<script>
+  import { drawKeypoints } from '~/utils/canvas'
+  import { loadVideo } from '~/utils/video';
+  import { INPUT_OPTIONS, SINGLE_POSE_OPTIONS, OUTPUT_OPTIONS } from '~/constants';
+
+  export default {
+    name: 'detect-canvas',
+    data() {
+      return {
+        video: null,
+        canvas: null,
+        net: null,
+        stopTrading: true
+      }
+    },
+    components: {
+    },
+    methods: {
+      reset() {
+        this.stopTrading = true;
+      },
+      async poseDetectionFrame() {
+        if (!this.stopTrading) {
+          const ctx = this.canvas.getContext('2d');
+          const flipPoseHorizontal = true;
+          let poses = [];
+
+          // ctx.clearRect(0, 0, this.video.width, this.video.height);
+          ctx.save();
+          ctx.scale(-1, 1);
+          ctx.translate(-this.video.width, 0);
+          ctx.drawImage(this.video, 0, 0, this.video.width, this.video.height);
+          ctx.restore();
+
+          const pose = await this.net.estimatePoses(this.video, {
+            flipHorizontal: flipPoseHorizontal,
+            decodingMethod: 'single-person'
+          });
+
+          if (pose && pose.length && pose[0].score > 0.7) {
+            poses = poses.concat(pose);
+            const score = pose[0].score;
+            const keypoints = pose[0].keypoints;
+            const trakingPoses = {};
+
+            await poses.forEach(({score, keypoints}) => {
+              drawKeypoints(keypoints, ctx);
+            });
+
+            Object.keys(keypoints).forEach(key => {
+              const keypoint = keypoints[key];
+              if (keypoint.part === 'rightWrist') {
+                if (score > 0.56) {
+                  trakingPoses['rightWrist'] = keypoint;
+                }
+              } else if (keypoint.part === 'leftWrist') {
+                if (score > 0.56) {
+                  trakingPoses['leftWrist'] = keypoint;
+                }
+              } else if (keypoint.part === 'rightElbow') {
+                if (score > 0.6) {
+                  trakingPoses['rightElbow'] = keypoint;
+                }
+              } else if (keypoint.part === 'leftElbow') {
+                if (score > 0.6) {
+                  trakingPoses['leftElbow'] = keypoint;
+                }
+              } else if (keypoint.part === 'rightShoulder') {
+                if (score > 0.6) {
+                  trakingPoses['rightShoulder'] = keypoint;
+                }
+              } else if (keypoint.part === 'leftShoulder') {
+                if (score > 0.6) {
+                  trakingPoses['leftShoulder'] = keypoint;
+                }
+              }
+            });
+            this.detectPostion(trakingPoses);
+          }
+        }
+        requestAnimationFrame(this.poseDetectionFrame);
+      },
+      detectPoseInRealTime() {
+        this.poseDetectionFrame();
+      },
+      detectPostion(trakingPoses) {
+        if (trakingPoses) {
+          let rightWristScore = 0;
+          let leftWristScore = 0;
+          let rightWristPosition = null;
+          let leftWristPosition = null;
+          // 右手
+          if (trakingPoses['rightWrist']) {
+            rightWristScore = trakingPoses['rightWrist'].score || 0;
+            rightWristPosition = trakingPoses['rightWrist'].position;
+          }
+          // 左手
+          if (trakingPoses['leftWrist']) {
+            leftWristScore = trakingPoses['leftWrist'].score || 0;
+            leftWristPosition = trakingPoses['leftWrist'].position;
+          }
+        }
+      }
+    },
+    async mounted() {
+      const bodyEl = this.$refs.bodyEl;
+      this.video = this.$refs.videoEl;
+      this.canvas = this.$refs.canvasEl;
+      this.canvas.width = bodyEl.offsetWidth;
+      this.canvas.height = bodyEl.offsetHeight;
+      this.video.width = bodyEl.offsetWidth;
+      this.video.height = bodyEl.offsetHeight;
+
+      this.net = await posenet.load(INPUT_OPTIONS);
+      if (this.net && this.video  && this.canvas) {
+        this.stopTrading = false;
+        try {
+          this.video = await loadVideo(this.video);
+          this.detectPoseInRealTime();
+        } catch (e) {
+          throw e;
+        }
+      }
+
+      window.addEventListener('keydown', (e) => {
+        if (e.key.toLowerCase() === 'd') {
+          this.isDebug = !this.isDebug
+        }
+      })
+    }
+  }
+</script>
+
+<style lang="sass">
+  .detection-body,
+  .detection-video,
+  .detection-canvas
+    position: absolute
+    box-sizing: border-box
+    display: block
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    padding: 0
+    margin: 0
+    width: 100%
+    height: 100%
+    overflow: hidden
+    z-index: 1
+  .detection-video
+    display: none
+    z-index: -1
+  .detection-canvas
+    display: block
+    z-index: 1
+    opacity: 0
+    transition: opacity .38s ease-in-out
+    &.isDebug
+      opacity: 1
+</style>
--- utils/video.js
+++ utils/video.js
@@ -1,27 +1,36 @@
 export const setupCamera = async (videoEl) => {
-    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-        throw new Error('Browser API navigator.mediaDevices.getUserMedia not available');
-    }
-    const deviceId = '35a58b5f0a2fa20dfb6f26c41eb07d0824b31e7f3aab3a7de5ff8f4fb51ebc1d'
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw new Error('Browser API navigator.mediaDevices.getUserMedia not available');
+  }
 
-    const stream = await navigator.mediaDevices.getUserMedia({
-        'audio': false,
-        'video': {
-          facingMode: 'user',
-          // deviceId: deviceId
-        },
-    });
-    videoEl.srcObject = stream;
+  let deviceList = [];
+  await navigator.mediaDevices.enumerateDevices().then(devices => {
+    deviceList = devices.filter(device => {
+      return device.kind === 'videoinput'
+    })
+  }).catch(err => {
+    throw new Error('Browser API navigator.mediaDevices.getUserMedia not available');
+  });
 
-    return new Promise((resolve) => {
-        videoEl.onloadedmetadata = () => {
-            resolve(videoEl);
-        };
-    });
+  // const deviceId = '35a58b5f0a2fa20dfb6f26c41eb07d0824b31e7f3aab3a7de5ff8f4fb51ebc1d'
+
+  const stream = await navigator.mediaDevices.getUserMedia({
+    'audio': false,
+    'video': {
+      deviceId: deviceList[0].deviceId
+    },
+  });
+  videoEl.srcObject = stream;
+
+  return new Promise((resolve) => {
+    videoEl.onloadedmetadata = () => {
+        resolve(videoEl);
+    };
+  });
 };
 
 export const loadVideo = async (videoEl) => {
-    const video = await setupCamera(videoEl);
-    video.play();
-    return video;
+  const video = await setupCamera(videoEl);
+  video.play();
+  return video;
 };
