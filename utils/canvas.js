71c79d6b471d9da15003bcc013fb310148479c76
Zeng Qihao <zengqihao0104@gmail.com>
2019-07-18 15:32:52 +0800

    dev

--- utils/canvas.js
+++ utils/canvas.js
@@ -1,35 +1,82 @@
+const isAndroid = () => {
+  return /Android/i.test(navigator.userAgent);
+};
+
+const isiOS = () => {
+  return /iPhone|iPad|iPod/i.test(navigator.userAgent);
+};
+
+export const isMobile = () => {
+  return isAndroid() || isiOS();
+};
+
 const color = 'aqua';
 const boundingBoxColor = 'red';
 const lineWidth = 2;
 
-const drawPoints = (ctx, points, radius, color) => {
-    const data = points.buffer().values;
-
-    for (let i = 0; i < data.length; i += 2) {
-        const pointY = data[i];
-        const pointX = data[i + 1];
-
-        if (pointX !== 0 && pointY !== 0) {
-            ctx.beginPath();
-            ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
-            ctx.fillStyle = color;
-            ctx.fill();
-        }
-    }
+const toTuple = ({y, x}) => {
+  return [y, x];
 };
 
 const drawPoint = (ctx, y, x, r, color) => {
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2 * Math.PI);
-    ctx.fillStyle = color;
-    ctx.fill();
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, 2 * Math.PI);
+  ctx.fillStyle = color;
+  ctx.fill();
+};
+
+export const drawSegment = ([ay, ax], [by, bx], color, scale, ctx) => {
+  ctx.beginPath();
+  ctx.moveTo(ax * scale, ay * scale);
+  ctx.lineTo(bx * scale, by * scale);
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+};
+
+export const drawSkeleton = (keypoints, minConfidence, ctx, scale = 1, posenet) => {
+  const adjacentKeyPoints =
+    posenet.getAdjacentKeyPoints(keypoints, minConfidence);
+
+  adjacentKeyPoints.forEach((keypoints) => {
+    drawSegment(
+      toTuple(keypoints[0].position), toTuple(keypoints[1].position), color,
+      scale, ctx);
+  });
 };
 
 export const drawKeypoints = (keypoints, ctx, scale = 1) => {
-    for (let i = 0; i < keypoints.length; i++) {
-        const keypoint = keypoints[i];
+  for (let i = 0; i < keypoints.length; i++) {
+    const keypoint = keypoints[i];
 
-        const { y, x } = keypoint.position;
-        drawPoint(ctx, y * scale, x * scale, 3, color);
+    const { y, x } = keypoint.position;
+    drawPoint(ctx, y * scale, x * scale, 3, color);
+  }
+};
+
+export const drawBoundingBox = (keypoints, ctx, posenet) => {
+  const boundingBox = posenet.getBoundingBox(keypoints);
+
+  ctx.rect(
+    boundingBox.minX, boundingBox.minY, boundingBox.maxX - boundingBox.minX,
+    boundingBox.maxY - boundingBox.minY);
+
+  ctx.strokeStyle = boundingBoxColor;
+  ctx.stroke();
+};
+
+const drawPoints = (ctx, points, radius, color) => {
+  const data = points.buffer().values;
+
+  for (let i = 0; i < data.length; i += 2) {
+    const pointY = data[i];
+    const pointX = data[i + 1];
+
+    if (pointX !== 0 && pointY !== 0) {
+      ctx.beginPath();
+      ctx.arc(pointX, pointY, radius, 0, 2 * Math.PI);
+      ctx.fillStyle = color;
+      ctx.fill();
     }
+  }
 };
